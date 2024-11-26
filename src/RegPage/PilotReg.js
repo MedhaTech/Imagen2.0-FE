9b4b4559f632fde153914f322d13526f5c95c911
shaikarshiya@inqui-lab.org <shaikarshiya@inqui-lab.org>
2024-11-26 16:25:20 +0530

    New

--- src/RegPage/PilotReg.js
+++ src/RegPage/PilotReg.js
@@ -114,10 +114,10 @@ const PilotReg = () => {
         <span style={{ color: "red" }}>Please Select Year of Study</span>
       ),
       password: Yup.string().required(
-        <span style={{ color: "red" }}>Please Select password</span>
+        <span style={{ color: "red" }}>Please Enter password</span>
       ),
       confirmPassword: Yup.string().required(
-        <span style={{ color: "red" }}>Please Select confirmPassword</span>
+        <span style={{ color: "red" }}>Please Enter confirmPassword</span>
       ),
     }),
 
