43d8493532c8afc0d1276b19621a1e4b1466bdf6
shaikarshiya@inqui-lab.org <shaikarshiya@inqui-lab.org>
2025-02-18 12:49:09 +0530

    New

--- src/components/IdeaSubmissionCard.jsx
+++ src/components/IdeaSubmissionCard.jsx
@@ -32,8 +32,8 @@ const LinkComponent = ({ item }) => {
     );
 };
 const IdeaSubmissionCard = ({ handleClose, show, response, props }) => {
- console.log(response,"response");
- const teamResponse = response;
+  const teamResponse = response;
+  console.log(teamResponse,"response");
   const [images, setImages] = useState([]);
    useEffect(() => {
       if (teamResponse) {
@@ -67,7 +67,7 @@ const IdeaSubmissionCard = ({ handleClose, show, response, props }) => {
                         id="contained-modal-title-vcenter"
                         className="w-100 d-block text-center"
                     >
-                        {response.theme}
+                        {response?.theme}
                     </Modal.Title>
                 </Modal.Header>
 
@@ -101,12 +101,12 @@ const IdeaSubmissionCard = ({ handleClose, show, response, props }) => {
                         color: "black",
                       }}
                     >
-                      {teamResponse.theme}
+                      {teamResponse?.theme}
                     </p>
                   </div>
                 </div>
               </div>
-             {theme === "Others" &&( <div className="col-lg-12 order-lg-0 order-1 p-0 h-100">
+             {teamResponse?.theme === "Others" &&( <div className="col-lg-12 order-lg-0 order-1 p-0 h-100">
                 <div
                   // key={index}
                   className="mb-4 my-3 comment-card px-4 py-2 card me-md-3"
@@ -118,7 +118,7 @@ const IdeaSubmissionCard = ({ handleClose, show, response, props }) => {
                         marginBottom: "1rem",
                       }}
                     >
-                      {theme !== "Others" ? "" :"2"}. Describe the category your idea belongs to
+                      {teamResponse?.theme !== "Others" ? "" :"2"}. Describe the category your idea belongs to
                     </b>
                   </div>
                   <div
@@ -152,7 +152,7 @@ const IdeaSubmissionCard = ({ handleClose, show, response, props }) => {
                         marginBottom: "1rem",
                       }}
                     >
-                      {theme !== "Others" ? "2" :"3"}. Describe your idea (in one sentence).
+                      {teamResponse?.theme !== "Others" ? "2" :"3"}. Describe your idea (in one sentence).
                     </b>
                   </div>
                   <div
@@ -186,7 +186,7 @@ const IdeaSubmissionCard = ({ handleClose, show, response, props }) => {
                         marginBottom: "1rem",
                       }}
                     >
-                      {theme !== "Others" ? "3" :"4"}. Give a title to your idea.
+                      {teamResponse?.theme !== "Others" ? "3" :"4"}. Give a title to your idea.
                     </b>
                   </div>
                   <div
@@ -220,7 +220,7 @@ const IdeaSubmissionCard = ({ handleClose, show, response, props }) => {
                         marginBottom: "1rem",
                       }}
                     >
-                      {theme !== "Others" ? "4" :"5"}. What problem does your idea solve?
+                      {teamResponse?.theme !== "Others" ? "4" :"5"}. What problem does your idea solve?
                     </b>
                   </div>
                   <div
@@ -254,7 +254,7 @@ const IdeaSubmissionCard = ({ handleClose, show, response, props }) => {
                         marginBottom: "1rem",
                       }}
                     >
-                      {theme !== "Others" ? "5" :"6"}. Who are your target customers/users?
+                      {teamResponse?.theme !== "Others" ? "5" :"6"}. Who are your target customers/users?
                     </b>
                   </div>
                   <div
@@ -288,7 +288,7 @@ const IdeaSubmissionCard = ({ handleClose, show, response, props }) => {
                         marginBottom: "1rem",
                       }}
                     >
-                                                    {theme !== "Others" ? "6" :"7"}. Explain your idea in detail
+                                                    {teamResponse?.theme !== "Others" ? "6" :"7"}. Explain your idea in detail
                     </b>
                   </div>
                   <div
@@ -322,7 +322,7 @@ const IdeaSubmissionCard = ({ handleClose, show, response, props }) => {
                         marginBottom: "1rem",
                       }}
                     >
-                     {theme !== "Others" ? "7" :"8"}. What stage is your idea currently at?
+                     {teamResponse?.theme !== "Others" ? "7" :"8"}. What stage is your idea currently at?
                     </b>
                   </div>
                   <div
@@ -363,7 +363,7 @@ const IdeaSubmissionCard = ({ handleClose, show, response, props }) => {
                         marginBottom: "1rem",
                       }}
                     >
-                      {theme !== "Others" ? "8" :"9"}. How unique is your idea compared to existing solutions?
+                      {teamResponse?.theme !== "Others" ? "8" :"9"}. How unique is your idea compared to existing solutions?
                     </b>
                   </div>
                   <div
@@ -405,7 +405,7 @@ const IdeaSubmissionCard = ({ handleClose, show, response, props }) => {
                         marginBottom: "1rem",
                       }}
                     >
-                      {theme !== "Others" ? "9" :"10"}. Who are your competitors or similar ideas?
+                      {teamResponse?.theme !== "Others" ? "9" :"10"}. Who are your competitors or similar ideas?
                     </b>
                   </div>
                   <div
@@ -439,7 +439,7 @@ const IdeaSubmissionCard = ({ handleClose, show, response, props }) => {
                         marginBottom: "1rem",
                       }}
                     >
-                      {theme !== "Others" ? "10" :"11"}. How will your idea make revenue or sustain itself?
+                      {teamResponse?.theme !== "Others" ? "10" :"11"}. How will your idea make revenue or sustain itself?
                     </b>
                   </div>
                   <div
@@ -473,7 +473,7 @@ const IdeaSubmissionCard = ({ handleClose, show, response, props }) => {
                         marginBottom: "1rem",
                       }}
                     >
-                      {theme !== "Others" ? "11" :"12"}. What impact will your idea have on society or the
+                      {teamResponse?.theme !== "Others" ? "11" :"12"}. What impact will your idea have on society or the
                       environment?
                     </b>
                   </div>
@@ -508,7 +508,7 @@ const IdeaSubmissionCard = ({ handleClose, show, response, props }) => {
                         marginBottom: "1rem",
                       }}
                     >
-                      {theme !== "Others" ? "12" :"13"}. How confident are you in your ability to implement
+                      {teamResponse?.theme !== "Others" ? "12" :"13"}. How confident are you in your ability to implement
                       your idea with your current skill set?
                     </b>
                   </div>
@@ -553,7 +553,7 @@ const IdeaSubmissionCard = ({ handleClose, show, response, props }) => {
                         marginBottom: "1rem",
                       }}
                     >
-                      {theme !== "Others" ? "13" :"14"}. What additional support and resources would you need
+                      {teamResponse?.theme !== "Others" ? "13" :"14"}. What additional support and resources would you need
                       to implement or get started with your idea ?
                     </b>
                   </div>
@@ -609,7 +609,7 @@ const IdeaSubmissionCard = ({ handleClose, show, response, props }) => {
                         marginBottom: "1rem",
                       }}
                     >
-                      {theme !== "Others" ? "14" :"15"}. Upload images/documents & video links related to your
+                      {teamResponse?.theme !== "Others" ? "14" :"15"}. Upload images/documents & video links related to your
                       Idea.(total size limit : 10 MB)
                     </b>
                   </div>
