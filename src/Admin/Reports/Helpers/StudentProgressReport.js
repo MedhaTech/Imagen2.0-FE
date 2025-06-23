545f1388e17d8c49adad5e763ea4bf60fb8b1df4
shaikarshiya@inqui-lab.org <shaikarshiya@inqui-lab.org>
2025-06-23 15:59:11 +0530

    New

--- src/Admin/Reports/Helpers/StudentProgressReport.js
+++ src/Admin/Reports/Helpers/StudentProgressReport.js
@@ -125,6 +125,18 @@ const StudentProgress = () => {
       label: "Gender",
       key: "gender",
     },
+     {
+      label: "Date of Birth",
+      key: "dateofbirth",
+    },
+    {
+      label: "Disability Status",
+      key: "disability",
+    },
+    {
+      label: "Area of Residence",
+      key: "area",
+    },
     {
       label: "Email Address",
       key: "email",
