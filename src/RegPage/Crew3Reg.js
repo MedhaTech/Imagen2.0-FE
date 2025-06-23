ce94b7d38ee24587b52042021e4d8955c5326f38
Ramant <ramant@medhatech.in>
2025-06-23 18:10:04 +0530

    lable name change for new fields

--- src/RegPage/Crew3Reg.js
+++ src/RegPage/Crew3Reg.js
@@ -235,10 +235,10 @@ const Crew3Reg = () => {
               <span style={{ color: "red" }}>Please Select Date of Birth</span>
             ),
             disability: Yup.string().required(
-              <span style={{ color: "red" }}>Please Select Disability</span>
+              <span style={{ color: "red" }}>Please Select Disability Status</span>
             ),
             area: Yup.string().required(
-              <span style={{ color: "red" }}>Please Select Residential Area Type</span>
+              <span style={{ color: "red" }}>Please Select Area of Residence</span>
             )
     }),
 
@@ -515,7 +515,7 @@ const Crew3Reg = () => {
                                             </div>
                                             <div className={`col-md-4`}>
                                               <label htmlFor="disability" className="form-label">
-                                                Do you have any disability?
+                                                Disability Status
                                               </label>
                                               &nbsp;
                                               <span style={{ color: "red", fontWeight: "bold" }}>
@@ -530,7 +530,7 @@ const Crew3Reg = () => {
                                                 onBlur={formik.handleBlur}
                                                 onChange={formik.handleChange}
                                               >
-                                                <option value={""}>Do you have any disability?</option>
+                                                <option value={""}>Disability Status</option>
                                                 {disabilityList.map((item) => (
                                                   <option key={item} value={item}>
                                                     {item}
@@ -545,7 +545,7 @@ const Crew3Reg = () => {
                                             </div>
                                             <div className={`col-md-4`}>
                                               <label htmlFor="area" className="form-label">
-                                                Your Residential Area Type
+                                                Area of Residence
                                               </label>
                                               &nbsp;
                                               <span style={{ color: "red", fontWeight: "bold" }}>
@@ -560,7 +560,7 @@ const Crew3Reg = () => {
                                                 onBlur={formik.handleBlur}
                                                 onChange={formik.handleChange}
                                               >
-                                                <option value={""}>Your Residential Area Type</option>
+                                                <option value={""}>Area of Residence</option>
                                                 {areaList.map((item) => (
                                                   <option key={item} value={item}>
                                                     {item}
